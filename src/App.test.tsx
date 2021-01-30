d4c5b9faac6948a37d4f29908f6fb2b89385bea2
Rafaela Silva <rafalucy3@gmail.com>
2021-01-30 20:01:27 +0000

    Fix test error with right imports

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
+  const greetings = screen.queryByText("Hello World");
+  expect(greetings).toBeInTheDocument();
 });
