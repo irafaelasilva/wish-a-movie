f7615fd923defbbce342831db849cf167a563056
Rafaela Silva <rafalucy3@gmail.com>
2021-02-27 23:02:24 +0000

    Finishes sidebar

--- src/components/sidebar/menu/styles.ts
+++ src/components/sidebar/menu/styles.ts
@@ -6,14 +6,17 @@ export const MenuItemContainer = styled.div`
   margin-left: 15px;
   color: #7e7e7e;
   padding: 15px;
+  &:hover {
+    color: #3ed6da;
+  }
 `;
 
 export const ItemIcon = styled.div`
   display: flex;
   margin-right: 5px;
+  line-height: 16px;
 `;
+
 export const ItemText = styled.p`
   display: flex;
-  line-height: 20px;
-  vertical-align: middle;
 `;
